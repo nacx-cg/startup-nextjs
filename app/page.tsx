109e93f7ffe8d34864d2d064737354330ad2a789
nacx-cg <130256807+nacx-cg@users.noreply.github.com>
2023-07-31 21:40:51 -0600

    version de bulbo website

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Features from "@/components/Features";
 import Hero from "@/components/Hero";
 import Pricing from "@/components/Pricing";
 import Testimonials from "@/components/Testimonials";
-import Video from "@/components/Video";
+
 import { Inter } from "@next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,13 +19,9 @@ export default function Home() {
       <ScrollUp />
       <Hero />
       <Features />
-      <Video />
-      <Brands />
+
       <AboutSectionOne />
       <AboutSectionTwo />
-      <Testimonials />
-      <Pricing />
-      <Blog />
       <Contact />
     </>
   );
--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -54,15 +54,15 @@ const Header = () => {
                 } `}
               >
                 <Image
-                  src="/images/logo/logo-2.svg"
-                  alt="logo"
+                  src=""
+                  alt="BULBO"
                   width={140}
                   height={30}
                   className="w-full dark:hidden"
                 />
                 <Image
-                  src="/images/logo/logo.svg"
-                  alt="logo"
+                  src=""
+                  alt="BULBO"
                   width={140}
                   height={30}
                   className="hidden w-full dark:block"
@@ -122,7 +122,7 @@ const Header = () => {
                                 <svg width="15" height="14" viewBox="0 0 15 14">
                                   <path
                                     d="M7.81602 9.97495C7.68477 9.97495 7.57539 9.9312 7.46602 9.8437L2.43477 4.89995C2.23789 4.70308 2.23789 4.39683 2.43477 4.19995C2.63164 4.00308 2.93789 4.00308 3.13477 4.19995L7.81602 8.77183L12.4973 4.1562C12.6941 3.95933 13.0004 3.95933 13.1973 4.1562C13.3941 4.35308 13.3941 4.65933 13.1973 4.8562L8.16601 9.79995C8.05664 9.90933 7.94727 9.97495 7.81602 9.97495Z"
-                                    fill="currentColor"
+                                    fill=""
                                   />
                                 </svg>
                               </span>
